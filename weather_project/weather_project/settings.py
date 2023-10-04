df00646aaa3811fc63b6c38e3325ee338a5f1e5f
Renat Mukanov <103631832+Fenokit@users.noreply.github.com>
2023-10-05 02:35:59 +0400

    Configured 'settings.py ', namely added virtual environment variables

--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,13 +73,26 @@ WSGI_APPLICATION = 'weather_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+env = os.environ.get('DJANGO_ENVIRONMENT')
+if env == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('name_table'),
+            'USER': os.environ.get('user_db'),
+            'PASSWORD': os.environ.get('password_db'),
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
